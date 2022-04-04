5e14d47657fbc4889abb118def540e842bc66c78
Flood123789 <73972594+Flood123789@users.noreply.github.com>
2022-04-04 16:34:12 -0400

    CSS linked

--- Hotspot Fruit/cart.php
+++ Hotspot Fruit/cart.php
@@ -16,6 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
     
