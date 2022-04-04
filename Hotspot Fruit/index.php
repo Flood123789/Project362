67f5c6a060fc52f0c807aa6d43c9f8987eeb2564
Flood123789 <73972594+Flood123789@users.noreply.github.com>
2022-04-04 16:30:33 -0400

    New Pages Created

--- Hotspot Fruit/index.php
+++ Hotspot Fruit/index.php
@@ -22,10 +22,10 @@
 </head>
 <body>
     <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a class="active" href="index.php">Home</a>
+        <a href="popular.php">Popular picks</a>
+        <a href="Cart.php">Cart</a>
+        <a href="#about.php">About</a>
     </div>
 
     <h2>Hotspot Fruit</h2>
