a6eeb11f2034be34a64bc9bf8e6d4c5274d8c31a
Flood123789 <73972594+Flood123789@users.noreply.github.com>
2022-04-04 15:35:00 -0400

    Directory Change

--- Hotspot Fruit/index.php
+++ Hotspot Fruit/index.php
@@ -13,9 +13,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotspot Fruit</title>
 </head>
 <body>
-    
+    <h2>Hotspot Fruit</h2>
+    <main>
+
+    </main>                                                                                                          
 </body>
 </html>
