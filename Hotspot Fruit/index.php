a2762c2f804fcd61053b12564181201fa7741128
Flood123789 <73972594+Flood123789@users.noreply.github.com>
2022-04-04 15:42:59 -0400

    Filling out main page

--- Hotspot Fruit/index.php
+++ Hotspot Fruit/index.php
@@ -1,4 +1,6 @@
 <?php
+    // Will check Database avalability.
+
     require_once('database.php');
     /*$FillTable = "SELECT * FROM students";   
     $Fulltable = $db->query($FillTable);
@@ -14,10 +16,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotspot Fruit</title>
+
+    <link rel="stylesheet" type="text/css" href="main.css">
+
 </head>
 <body>
     <h2>Hotspot Fruit</h2>
     <main>
+        <span>Test Text</span>
 
     </main>                                                                                                          
 </body>
